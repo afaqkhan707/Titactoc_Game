b326860542ef78b7cffb6827d2b4f122cf6b2125
afaqkhan707 <bsse5482@gmail.com>
2023-09-19 12:39:26 +0500

    Ticttactoe Game in Next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
 import Image from "next/image";
-// import styles from "./page.module.css";
-import Navbar from "./components/Navbar";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-import Navigation from "./components/Navigation";
 import Tictactoe from "./components/Tictactoe";
 
 
@@ -12,7 +7,7 @@ export default function Home() {
 
   return (
     <>
- <Navbar/>
+ <Tictactoe/>
     </>
   );
 }
